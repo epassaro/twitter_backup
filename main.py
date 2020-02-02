2fa3dd8770c3e75710393991a6006468f44c59bc
Ezequiel Pássaro <epassaro15@gmail.com>
2020-02-02 19:00:53 -0300

    Fixed typo

--- main.py
+++ main.py
@@ -6,12 +6,7 @@ CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
 ACCESS_TOKEN_KEY = os.environ['ACCESS_TOKEN_KEY']
 ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
 
-print(CONSUMER_KEY[-5:])
-print(CONSUMER_SECRET[-5:])
-print(ACCESS_TOKEN_KEY[-5:])
-print(ACCESS_TOKEN_SECRET[-5:])
-
-api = twitter.Api(consumer_key=CONSUMER_SECRET,
+api = twitter.Api(consumer_key=CONSUMER_KEY,
                   consumer_secret=CONSUMER_SECRET,
                   access_token_key=ACCESS_TOKEN_KEY,
                   access_token_secret=ACCESS_TOKEN_SECRET)
