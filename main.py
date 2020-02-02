94a9b0d91ae7f57bb028fe4cd16ca78c26dcec1b
Ezequiel Pássaro <epassaro15@gmail.com>
2020-02-02 20:23:07 -0300

    Set cron again. Use `if __name__ ...`

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
 ACCESS_TOKEN_KEY = os.environ['ACCESS_TOKEN_KEY']
 ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
 
-api = twitter.Api(consumer_key=CONSUMER_KEY,
-                  consumer_secret=CONSUMER_SECRET,
-                  access_token_key=ACCESS_TOKEN_KEY,
-                  access_token_secret=ACCESS_TOKEN_SECRET)
 
-friends = api.GetFriends(user_id='epassaro')
-friends_df = pd.DataFrame(friends, columns=['friends'])
-friends_df.to_csv('friends.csv')
+if __name__ == "__main__":
+    api = twitter.Api(consumer_key=CONSUMER_KEY,
+                      consumer_secret=CONSUMER_SECRET,
+                      access_token_key=ACCESS_TOKEN_KEY,
+                      access_token_secret=ACCESS_TOKEN_SECRET)
+
+    friends = api.GetFriends(user_id='epassaro')
+    friends_df = pd.DataFrame(friends, columns=['friends'])
+    friends_df.to_csv('friends.csv')
